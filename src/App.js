129e19ab358163cdfbe851f1786f139a1c8bde28
tanulka <tanulkako@gmail.com>
2021-10-30 22:11:19 +0300

    hw 1 task logic done

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './App.css';
+import user from './user.json';
+import Profile from './components/Profile';
+
+function App() {
+  return (
+    <Profile
+      name={user.name}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+  );
+}
+
+export default App;
